5cafd40e5c6999909d0bec2db2892bfa2f9bce58
Fazle Bari <fazlebarisn@gmail.com>
2024-01-11 16:26:20 +0600

    remove dd function

--- functions.php
+++ functions.php
@@ -1,24 +1,5 @@
 <?php
 /**
- * Only for developer
- * @author Fazle Bari
- */
-if( ! function_exists('dd') ){
-    function dd( ...$vals){
-        if( ! empty($vals) && is_array($vals) ){
-            ob_start(); // Start output buffering
-            foreach($vals as $val ){
-                echo "<pre>";
-                    var_dump($val);
-                echo "</pre>";
-            }
-            $output = ob_get_clean(); // Get the buffered output and clear the buffer
-            echo $output; // Output the buffered content
-        }
-    }
-}
-
-/**
  * Add main admin page
  */
 function adminMenu(){
